08b45928ac29f44060b1091895a43e70b48d3ded
Adriaan Meuris <adriaan@spectrum.io>
2018-01-08 06:31:14 +0100

    use 1 build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd src/tabs
+
+cat cff.js  cmap.js  glyf.js  GPOS.js  GSUB.js  head.js  hhea.js  \
+hmtx.js  kern.js  loca.js  maxp.js  name.js  os-2.js  post.js  >  tabs.js
+
+cd ..
+
+cat  main.js  bin.js  lctf.js  tabs/tabs.js  > ../dist/Typr.js
