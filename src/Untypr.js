862650d4aa1d00a9041d4f4c52e49e069e54b4d0
Vaclav Lunak <v.lunak@gmail.com>
2021-09-06 16:15:15 +0200

    changed write method signature

--- src/Untypr.js
+++ src/Untypr.js
@@ -4,37 +4,37 @@ var Untypr = {};
 Untypr["T"] = {};
 
 Untypr["T"].head = {
-    encodeTab: function(obj, offset, buff)
+    encodeTab: function(obj)
     {
         var bin = Untypr["B"];
-        bin.writeUint16(buff, offset, 1); offset += 2; // majorVersion
-        bin.writeUint16(buff, offset, 0); offset += 2; // minorVersion
-        bin.writeFixed(buff, offset, obj["fontRevision"]); offset += 4;
-        var checkSumOffset = offset; // TODO
-        bin.writeUint(buff, offset, 0); offset += 4; // checkSumAdjustment
-        bin.writeUint(buff, offset, 0x5f0f3cf5); offset += 4; // magic constant
-        bin.writeUint(buff, offset, obj["flags"]); offset += 2;
-        bin.writeUshort(buff, offset, obj["unitsPerEm"]); offset += 2;
-        bin.writeUint64(buff, offset, obj["created"]); offset += 8;
-        bin.writeUint64(buff, offset, obj["modified"]); offset += 8;
-        bin.writeShort(buff, offset, obj["xMin"]); offset += 2;
-        bin.writeShort(buff, offset, obj["yMin"]); offset += 2;
-        bin.writeShort(buff, offset, obj["xMax"]); offset += 2;
-        bin.writeShort(buff, offset, obj["yMax"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["macStyle"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["lowestRecPPEM"]); offset += 2;
-        bin.writeShort(buff, offset, obj["fontDirectionHint"]); offset += 2;
-        bin.writeShort(buff, offset, obj["indexToLocFormat"]); offset += 2;
-        bin.writeShort(buff, offset, obj["glyphDataFormat"]);
+        bin.writeUint16(1); // majorVersion
+        bin.writeUint16(0); // minorVersion
+        bin.writeFixed(obj["fontRevision"]);
+        var checkSumOffset = bin.getCurrentOffset(); // TODO
+        bin.writeUint(0); // checkSumAdjustment
+        bin.writeUint(0x5f0f3cf5); // magic constant
+        bin.writeUint(obj["flags"]);
+        bin.writeUshort(obj["unitsPerEm"]);
+        bin.writeUint64(obj["created"]);
+        bin.writeUint64(obj["modified"]);
+        bin.writeShort(obj["xMin"]);
+        bin.writeShort(obj["yMin"]);
+        bin.writeShort(obj["xMax"]);
+        bin.writeShort(obj["yMax"]);
+        bin.writeUint16(obj["macStyle"]);
+        bin.writeUint16(obj["lowestRecPPEM"]);
+        bin.writeShort(obj["fontDirectionHint"]);
+        bin.writeShort(obj["indexToLocFormat"]);
+        bin.writeShort(obj["glyphDataFormat"]);
     }
 };
 
 Untypr["T"].hhea = {
-    encodeTab: function(obj, offset, buff)
+    encodeTab: function(obj)
     {
         var bin = Untypr["B"];
-        bin.writeUint16(buff, offset, 1); offset += 2;// major version;
-        bin.writeUint16(buff, offset, 0); offset += 2;// minor version;
+        bin.writeUint16(1); // major version;
+        bin.writeUint16(0); // minor version;
 
         var keys = ["ascender","descender","lineGap",
             "advanceWidthMax","minLeftSideBearing","minRightSideBearing","xMaxExtent",
@@ -44,42 +44,42 @@ Untypr["T"].hhea = {
 
         for(var i=0; i<keys.length; i++) {
             var key = keys[i];
-            if (!key) bin.writeShort(buff,offset+i*2,0); // reserved
+            if (!key) bin.writeShort(0); // reserved
             else {
                 var func = (key=="advanceWidthMax" || key=="numberOfHMetrics")?bin.writeUint16:bin.writeShort;
-                func(buff, offset+i*2, obj[key]);
+                func(obj[key]);
             }
         }
     }
 };
 
 Untypr["T"].hmtx = {
-    encodeTab: function(obj, offset, buff)
+    encodeTab: function(obj)
     {
         var bin = Untypr["B"];
         var nH = obj["aWidth"].length;
         var nG = obj["lsBearing"].length;
         var i = 0;
         while (i<nH) {
-            bin.writeUint16(buff, offset, obj["aWidth"][i]); offset += 2;
-            bin.writeShort(buff, offset, obj["lsBearing"][i]); offset += 2;
+            bin.writeUint16(obj["aWidth"][i]);
+            bin.writeShort(obj["lsBearing"][i]);
             i++;
         }
         while (i<nG) {
-            bin.writeShort(buff, offset, obj["lsBearing"][i]); offset += 2;
+            bin.writeShort(obj["lsBearing"][i]);
             i++;
         }
     }
 }
 
 Untypr["T"].maxp = {
-    encodeTab: function(obj, offset, buff)
+    encodeTab: function(obj)
     {
         var bin = Untypr["B"];
         // FIXME: Typr doesn't store version information
         var ver = obj["version"] ? obj["version"] << 16 : 0x00005000
-        bin.writeUint(buff, offset, ver); offset += 4;
-        bin.writeUint16(buff, offset, obj["numGlyphs"]);
+        bin.writeUint(ver);
+        bin.writeUint16(obj["numGlyphs"]);
 
         if (ver == 0x00010000) {
             var keys = ["maxPoints", "maxContours", "maxCompositePoints", "maxCompositeContours",
@@ -87,20 +87,20 @@ Untypr["T"].maxp = {
             "maxStackElements", "maxSizeOfInstructions", "maxComponentElements", "maxComponentDepth"];
 
             for (var i=0; i<keys.length; i++) {
-                bin.writeUint16(buff, offset, obj[keys[i]]); offset += 2;
+                bin.writeUint16(obj[keys[i]]);
             }
         }
     }
 };
 
 Untypr["T"].name = {
-    encodeTab: function(obj, offset, buff)
+    encodeTab: function(obj)
     {
         var bin = Untypr["B"];
         // Typr doesn't support v1, so store only v0
-        bin.writeShort(buff, offset, 0); offset += 2;
-        bin.writeShort(buff, offset, obj["count"]); offset += 2;
-        bin.writeShort(buff, offset, 0); // TODO: add offset
+        bin.writeShort(0);
+        bin.writeShort(obj["count"]);
+        bin.writeShort(0); // TODO: add offset
 
         var names = [
             "copyright",
@@ -163,23 +163,23 @@ Untypr["T"].name = {
         });
 
         for (var r of records) {
-            bin.writeUint16(buff, offset, r.pID); offset += 2;
-            bin.writeUint16(buff, offset, r.eID); offset += 2;
-            bin.writeUint16(buff, offset, r.lID); offset += 2;
-            bin.writeUint16(buff, offset, r.nID); offset += 2;
-            bin.writeUint16(buff, offset, obj[r.key][names[r.nID]].length); offset += 2;
-            bin.writeUint16(buff, offset, 0); offset += 2; // TODO add offsets;
+            bin.writeUint16(r.pID);
+            bin.writeUint16(r.eID);
+            bin.writeUint16(r.lID);
+            bin.writeUint16(r.nID);
+            bin.writeUint16(obj[r.key][names[r.nID]].length);
+            bin.writeUint16(0); // TODO add offsets;
         }
 
         for (r of records) {
             var func = (r.pID == 1 && r.eID == 0) ? bin.writeASCII : bin.writeUnicode;
-            func(buff, offset, obj[r.key][names[r.nID]]);
+            func(obj[r.key][names[r.nID]]);
         }
     }
 }
 
 Untypr["T"].OS2 = {
-    encodeTab: function(obj, offset, buff) {
+    encodeTab: function(obj) {
         var bin = Untypr["B"];
         // Typr doesn't store table version, so we have to guess it from contents
         var version;
@@ -188,53 +188,53 @@ Untypr["T"].OS2 = {
         else if (obj.hasOwnProperty("ulCodePageRange1")) version = 1;
         else version = 0;
 
-        bin.writeUint16(buff, offset, version); offset += 2;
-        bin.writeShort(buff, offset, obj["xAvgCharWidth"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["usWeightClass"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["usWidthClass"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["fsType"]); offset += 2;
-        bin.writeShort(buff, offset, obj["ySubscriptXSize"]); offset += 2;
-        bin.writeShort(buff, offset, obj["ySubscriptYSize"]); offset += 2;
-        bin.writeShort(buff, offset, obj["ySubscriptXOffset"]); offset += 2;
-        bin.writeShort(buff, offset, obj["ySubscriptYOffset"]); offset += 2;
-        bin.writeShort(buff, offset, obj["ySuperscriptXSize"]); offset += 2;
-        bin.writeShort(buff, offset, obj["ySuperscriptYSize"]); offset += 2;
-        bin.writeShort(buff, offset, obj["ySuperscriptXOffset"]); offset += 2;
-        bin.writeShort(buff, offset, obj["ySuperscriptYOffset"]); offset += 2;
-        bin.writeShort(buff, offset, obj["yStrikeoutSize"]); offset += 2;
-        bin.writeShort(buff, offset, obj["yStrikeoutPosition"]); offset += 2;
-        bin.writeShort(buff, offset, obj["sFamilyClass"]); offset += 2;
-        bin.writeBytes(buff, offset, obj["panose"]);  offset += 10;
-        bin.writeUint(buff, offset, obj["ulUnicodeRange1"]);  offset += 4;
-        bin.writeUint(buff, offset, obj["ulUnicodeRange2"]);  offset += 4;
-        bin.writeUint(buff, offset, obj["ulUnicodeRange3"]);  offset += 4;
-        bin.writeUint(buff, offset, obj["ulUnicodeRange4"]);  offset += 4;
-        bin.writeASCII(buff, offset, obj["achVendID"]);  offset += 4;
-        bin.writeUint16(buff, offset, obj["fsSelection"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["usFirstCharIndex"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["usLastCharIndex"]); offset += 2;
-        bin.writeShort(buff, offset, obj["sTypoAscender"]); offset += 2;
-        bin.writeShort(buff, offset, obj["sTypoDescender"]); offset += 2;
-        bin.writeShort(buff, offset, obj["sTypoLineGap"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["usWinAscent"]); offset += 2;
-        bin.writeUint16(buff, offset, obj["usWinDescent"]); offset += 2;
+        bin.writeUint16(version);
+        bin.writeShort(obj["xAvgCharWidth"]);
+        bin.writeUint16(obj["usWeightClass"]);
+        bin.writeUint16(obj["usWidthClass"]);
+        bin.writeUint16(obj["fsType"]);
+        bin.writeShort(obj["ySubscriptXSize"]);
+        bin.writeShort(obj["ySubscriptYSize"]);
+        bin.writeShort(obj["ySubscriptXOffset"]);
+        bin.writeShort(obj["ySubscriptYOffset"]);
+        bin.writeShort(obj["ySuperscriptXSize"]);
+        bin.writeShort(obj["ySuperscriptYSize"]);
+        bin.writeShort(obj["ySuperscriptXOffset"]);
+        bin.writeShort(obj["ySuperscriptYOffset"]);
+        bin.writeShort(obj["yStrikeoutSize"]);
+        bin.writeShort(obj["yStrikeoutPosition"]);
+        bin.writeShort(obj["sFamilyClass"]);
+        bin.writeBytes(obj["panose"]);
+        bin.writeUint(obj["ulUnicodeRange1"]);
+        bin.writeUint(obj["ulUnicodeRange2"]);
+        bin.writeUint(obj["ulUnicodeRange3"]);
+        bin.writeUint(obj["ulUnicodeRange4"]);
+        bin.writeASCII(obj["achVendID"]);
+        bin.writeUint16(obj["fsSelection"]);
+        bin.writeUint16(obj["usFirstCharIndex"]);
+        bin.writeUint16(obj["usLastCharIndex"]);
+        bin.writeShort(obj["sTypoAscender"]);
+        bin.writeShort(obj["sTypoDescender"]);
+        bin.writeShort(obj["sTypoLineGap"]);
+        bin.writeUint16(obj["usWinAscent"]);
+        bin.writeUint16(obj["usWinDescent"]);
 
         if (version >= 1) {
-            bin.writeUint(buff, offset, obj["ulCodePageRange1"]); offset += 4;
-            bin.writeUint(buff, offset, obj["ulCodePageRange2"]); offset += 4;
+            bin.writeUint(obj["ulCodePageRange1"]);
+            bin.writeUint(obj["ulCodePageRange2"]);
         }
 
         if (version >= 2) {
-            bin.writeShort(buff, offset, obj["sxHeight"]); offset += 2;
-            bin.writeShort(buff, offset, obj["sCapHeight"]); offset += 2;
-            bin.writeUint16(buff, offset, obj["usDefault"]); offset += 2;
-            bin.writeUint16(buff, offset, obj["usBreak"]); offset += 2;
-            bin.writeUint16(buff, offset, obj["usMaxContext"]); offset += 2;
+            bin.writeShort(obj["sxHeight"]);
+            bin.writeShort(obj["sCapHeight"]);
+            bin.writeUint16(obj["usDefault"]);
+            bin.writeUint16(obj["usBreak"]);
+            bin.writeUint16(obj["usMaxContext"]);
         }
 
         if (version >= 5) {
-            bin.writeUint16(buff, offset, obj["usLowerOpticalPointSize"]); offset += 2;
-            bin.writeUint16(buff, offset, obj["usUpperOpticalPointSize"]);
+            bin.writeUint16(obj["usLowerOpticalPointSize"]);
+            bin.writeUint16(obj["usUpperOpticalPointSize"]);
         }
     }
 }
@@ -257,17 +257,21 @@ Untypr["T"].post = {
 }
 
 Untypr["B"] = {
-    writeUint: function(buff, p, n)
+    writeUint: function(n)
     {
-        buff[p] = (n>>24)&255; buff[p+1] = (n>>16)&255; buff[p+2] = (n>>8)&255; buff[p+3] = n&255;
+        this._out.push((n>>24)&255);
+        this._out.push((n>>16)&255);
+        this._out.push((n>>8)&255);
+        this._out.push(n&255);
     },
-    writeUint16: function(buff, p, n)
+    writeUint16: function(n)
     {
-        buff[p] = (n>>8)&255; buff[p+1] = n&255;
+        this._out.push((n>>8)&255);
+        this._out.push(n&255);
     },
-    writeUint8: function(buff, p, n)
+    writeUint8: function(n)
     {
-        buff[p] = n&255;
+        this._out.push(n&255);
     },
     writeUint64: function(buff, p, n)
     {
@@ -277,23 +281,23 @@ Untypr["B"] = {
     {
         var a = Untypr["B"].t.uint8;
         Untypr["B"].t.int32[0] = n;
-        buff[p] = a[3];
-        buff[p+1] = a[2];
-        buff[p+2] = a[1];
-        buff[p+3] = a[0];
+        this._out.push(a[3]);
+        this._out.push(a[2]);
+        this._out.push(a[1]);
+        this._out.push(a[0]);
     },
     writeShort: function(buff, p, n)
     {
         var a = Untypr["B"].t.uint8;
         Untypr["B"].t.int16[0] = n;
-        buff[p] = a[1];
-        buff[p+1] = a[0];
+        this._out.push(a[1]);
+        this._out.push(a[0]);
     },
     writeInt8: function(buff, p, n)
     {
         var a = Untypr["B"].t.uint8;
         Untypr["B"].t.int8[0] = n;
-        buff[p] = a[0];
+        this._out.push(a[0]);
     },
     writeInt64: function(buff, p, n)
     {
@@ -307,19 +311,19 @@ Untypr["B"] = {
     {
         // TODO
     },
-    writeBytes: function(buff, p, arr)
+    writeBytes: function(arr)
     {
-        for(var i=0; i<arr.length; i++) buff[p+i] = arr[i];
+        for(var i=0; i<arr.length; i++) this._out.push(arr[i]);
     },
-    writeASCII: function(buff, p, s)
+    writeASCII: function(s)
     {
         for(var i = 0; i < s.length; i++)
-            buff[p+i] = s.charCodeAt(i);
+            this._out.push(s.charCodeAt(i));
     },
-    writeUnicode: function(buff, p, s)
+    writeUnicode: function(s)
     {
         var arr = new TextEncoder().encode(s);
-        for (var i = 0; i < arr.length; i++) buff[p+i] = arr[i];
+        for (var i = 0; i < arr.length; i++) this._out.push(arr[i]);
     },
     t : function() {
         var ab = new ArrayBuffer(8);
@@ -332,6 +336,28 @@ Untypr["B"] = {
             int32  : new Int32Array (ab),
             uint32 : new Uint32Array(ab)
         }
-    }()
+    }(),
+    _out: {
+        arr: new Uint8Array(1000),
+        offset: 0,
+        write: function(byte, offset) {
+            var out = Untypr["B"]._out;
+            if (offset > out.offset) out.offset = offset;
+            if (offset >= out.arr.length) {
+                var ab = new ArrayBuffer(arr.length * 2);
+                var newArr = new Uint8Array(ab);
+                newArr.set(out.arr);
+                out.arr = newArr;
+            }
+            out.arr[offset] = byte;
+        },
+        push: function(byte) { 
+            var out = Untypr["B"]._out;
+            out.write(byte, out.offset + 1);
+        }
+    },
+    getCurrentOffset: function() {
+        return Untypr["B"]._out.offset;
+    }
 };
 
